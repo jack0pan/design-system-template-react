204928e3dab71dcb3deb1129d6c7f5124afaf15c
Pan Jie <panjie@growingio.com>
2022-02-21 16:54:22 +0800

    chore: upgrade storybook to 6.4.19
    Move component and story files to src.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,8 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  features: {
+    postcss: false,
+  },
   framework: '@storybook/react',
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
 };
